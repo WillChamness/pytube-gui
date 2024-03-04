ca32f12396d404e4d33198f2903a6eac845adbdd
WillChamness <wzchamness@gmail.com>
2024-03-03 21:47:18 -0500

    fix: make form generation cross platform
    use os.path module to join directories for Windows

--- generate_forms.py
+++ generate_forms.py
@@ -1,9 +1,16 @@
 """Genrate the Python boilerplate code for the QT form"""
 import glob
+import platform
+import os
 from PyQt6.uic import pyuic
 
+try:
+    os.mkdir("./pytube_gui/form_ui")
+except FileExistsError:
+    pass
+
 for ui_file in glob.glob("./qt-forms/*.ui"):
-    file_name = ui_file.split("/")[-1] # e.g. from "./qt-forms/form.ui", get "form.ui"
+    file_name = ui_file.split("\\" if platform.system() == "Windows" else "/")[-1] # e.g. from "./qt-forms/form.ui", get "form.ui"
     destination_name = file_name.split(".ui")[0] + ".py" # *.ui to *.py
-    pyuic.generate(ui_file, f"./pytube_gui/form_ui/{destination_name}", 0, None) # convert *.ui to *.py
+    pyuic.generate(ui_file, os.path.join(".", "pytube_gui", "form_ui", destination_name), 0, None) # convert *.ui to *.py
 
