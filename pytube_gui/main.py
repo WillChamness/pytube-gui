e01d443c7512a42e24ad828a63f1e8a0a19ca39c
WillChamness <wzchamness@gmail.com>
2024-03-06 23:37:52 -0500

    feat: add icon file and load icon at runtime

--- pytube_gui/main.py
+++ pytube_gui/main.py
@@ -1,10 +1,11 @@
-from PyQt6 import QtWidgets 
+from PyQt6 import QtWidgets, QtGui
 from .form_logic.pytube_form import PyTubeForm 
 
 def main():
     import sys
     app = QtWidgets.QApplication(sys.argv)
     pythonYTDownloaderDialog = QtWidgets.QDialog()
+    pythonYTDownloaderDialog.setWindowIcon(QtGui.QIcon("./pytube_gui/form_ui/pytube-logo.ico"))
     ui = PyTubeForm(pythonYTDownloaderDialog)
     pythonYTDownloaderDialog.show()
     sys.exit(app.exec())
