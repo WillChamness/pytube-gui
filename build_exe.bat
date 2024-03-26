4b6c94fb0761c337e82f66535413a4878e23af22
WillChamness <wzchamness@gmail.com>
2024-03-26 15:49:02 -0400

    build: add icon to executable

--- build_exe.bat
+++ build_exe.bat
@@ -1,3 +1,3 @@
 @echo off
-pyinstaller --onefile --windowed --name "Pytube-GUI-Onefile"  run.py
-pyinstaller --windowed --name "Pytube-GUI" run.py
+pyinstaller --onefile --windowed --icon=resources/pytube-logo.ico --name "Pytube-GUI-Onefile"  run.py
+pyinstaller --windowed --icon=resources/pytube-logo.ico --name "Pytube-GUI" --noconfirm run.py
